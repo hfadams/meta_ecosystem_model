5744db225e53a6613f3f09669921f0e2448d9e7f
Hannah Adams <67337692+hfadams@users.noreply.github.com>
2024-01-09 16:41:24 -0330

    Update stats_models.R

--- scripts/stats_models.R
+++ scripts/stats_models.R
@@ -1363,7 +1363,34 @@ formatted_model_output <- model_summary %>%
          "predictor" = "predictor_var", 
          )
 
-write.csv(formatted_model_output, "output/empirical_glm_results.csv", row.names=FALSE)
+# now format the results:
+# select all rows that I want to transpose, keeping id rows (park, site, model, model#)
+stats_estimates <- formatted_model_output %>%
+  select(-delta_aicc, 
+         -aicc, 
+         -log_likelihood, 
+         -adj_r2,
+         -k)
+
+# transpose using cast function
+stats_data_wide <- dcast(stats_estimates, extent+model+model_num~predictor, 
+                          value.var = "estimate")
+
+# select delta aic, log likelihood and merge with the column above
+stats_model_fit <- formatted_model_output %>% 
+  select(extent, 
+         model,
+         model_num,
+         k,
+         delta_aicc, 
+         log_likelihood, 
+         adj_r2) %>%
+  distinct()
+  
+merged_stats_data <- stats_model_fit %>%
+  full_join(stats_data_wide, by=c("extent", "model", "model_num"))
+
+write.csv(merged_stats_data, "output/empirical_glm_results.csv", row.names=FALSE)
 
 # 6) Plot results from the top models ----
 
